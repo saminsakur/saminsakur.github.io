c40a67a8a3eef18819a6e72554bdae13073035db
samin <saminsakur8@gmail.com>
2021-07-28 14:37:29 +0600

    Intial commit

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ let mouseCursor = document.querySelector('.cursor')
 
 $(document).ready(function() {
     $(window).scroll(function() {
-        console.log($(window).scrollTop());
         if ($(window).scrollTop() > 20) {
             $("header").addClass("sticky");
             $("h1").addClass("sticky-state");
@@ -11,6 +10,11 @@ $(document).ready(function() {
             $("header").removeClass("sticky");
             $("h1").removeClass("sticky-state");
             $(".menu-item").removeClass("sticky-state");
+        }
+        if ($(window).scrollTop() > 200) {
+            $(".scroll-down").addClass("no-display");
+        } else {
+            $(".scroll-down").removeClass("no-display");
         }
     });
     // script to toggle menu when menu icon pressed
