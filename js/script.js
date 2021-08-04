46818c8b7752a38cd3ebc891f971a581982683a6
samin <saminsakur8@gmail.com>
2021-08-04 11:53:16 +0600

    Added a background image

--- js/script.js
+++ js/script.js
@@ -2,12 +2,8 @@ $(document).ready(function() {
     $(window).scroll(function() {
         if ($(window).scrollTop() > 20) {
             $("header").addClass("sticky");
-            $("h1").addClass("sticky-state");
-            $(".menu-item").addClass("sticky-state");
         } else {
             $("header").removeClass("sticky");
-            $("h1").removeClass("sticky-state");
-            $(".menu-item").removeClass("sticky-state");
         }
         if ($(window).scrollTop() > 200) {
             $(".scroll-down").addClass("no-display");
